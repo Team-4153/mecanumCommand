23b5d267111e2345802a4d6c378d33310da111cb
4153student <4153student@64.106.47.220>
2014-01-11 15:33:48 -0700

    Formatted

--- src/com/team4153/OI.java
+++ src/com/team4153/OI.java
@@ -1,4 +1,3 @@
-
 package com.team4153;
 
 import com.team4153.commands.DriveWithJoystick;
@@ -12,12 +11,13 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+
     private static Joystick joystick;
     private static Button triggerButton;
-    
-    public OI(){
-	joystick = new Joystick(RobotMap.JOYSTICK_PORT);
-	triggerButton = new JoystickButton(joystick, RobotMap.JSBUTTON_TRIGGER);
+
+    public OI() {
+        joystick = new Joystick(RobotMap.JOYSTICK_PORT);
+        triggerButton = new JoystickButton(joystick, RobotMap.JSBUTTON_TRIGGER);
 //	triggerButton.whenPressed(new DriveWithJoystick());
     }
     //// CREATING BUTTONS
@@ -26,36 +26,31 @@ public class OI {
     // number it is.
     // Joystick stick = new Joystick(port);
     // Button button = new JoystickButton(stick, buttonNumber);
-    
+
     // Another type of button you can create is a DigitalIOButton, which is
     // a button or switch hooked up to the cypress module. These are useful if
     // you want to build a customized operator interface.
     // Button button = new DigitalIOButton(1);
-    
     // There are a few additional built in buttons you can use. Additionally,
     // by subclassing Button you can create custom triggers and bind those to
     // commands the same as any other Button.
-    
     //// TRIGGERING COMMANDS WITH BUTTONS
     // Once you have a button, it's trivial to bind it to a button in one of
     // three ways:
-    
     // Start the command when the button is pressed and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenPressed(new ExampleCommand());
-    
     // Run the command while the button is being held down and interrupt it once
     // the button is released.
     // button.whileHeld(new ExampleCommand());
-    
     // Start the command when the button is released  and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenReleased(new ExampleCommand());
     public static Joystick getJoystick() {
-	return joystick;
+        return joystick;
     }
-    public static Button getTriggerButton(){
-	return triggerButton;
+
+    public static Button getTriggerButton() {
+        return triggerButton;
     }
 }
-
