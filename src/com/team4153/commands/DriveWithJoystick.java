a192fde435a3413424217bfbbe506563f5744336
4153student <4153student@64.106.47.220>
2014-01-11 15:33:28 -0700

    Formatted

--- src/com/team4153/commands/DriveWithJoystick.java
+++ src/com/team4153/commands/DriveWithJoystick.java
@@ -1,43 +1,42 @@
-
 package com.team4153.commands;
 
 /**
  * Chassis drive command.
- * 
+ *
  * @author kpt
  */
 public class DriveWithJoystick extends CommandBase {
 
     public DriveWithJoystick() {
         // Use requires() here to declare subsystem dependencies
-	requires(chassis);
-	requires(gyroSensor);
-	System.out.println("** DriveWithJoystick const");
+        requires(chassis);
+        requires(gyroSensor);
+        System.out.println("** DriveWithJoystick const");
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-	
+
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-	chassis.mecanumDrive(oi.getJoystick(), gyroSensor.getAngle());
+        chassis.mecanumDrive(oi.getJoystick(), gyroSensor.getAngle());
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-	return false;
+        return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-	chassis.driveHalt();
+        chassis.driveHalt();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-	end();
+        end();
     }
 }
